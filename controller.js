4bcea76bc99d710db3493333cb0d692360842e02
Katalyst33 <katalyst33>
2019-05-24 20:33:32 +0100

    initial

---
+++ controller.js
@@ -0,0 +1,21 @@
+new Vue ({
+
+    el:'#app',
+    data:{
+        message:'Pricing Menu',
+        mStatus:'',
+        child:'',
+        total:'',
+        roomType:'',
+        counter:0
+
+    },
+
+    method:{
+
+    }
+
+})
+
+
+
