369721264d8a0e7b1c974839e66135bde5d5b7c7
Katalyst33 <katalyst33>
2019-05-28 21:00:46 +0100

    compute total calculations on any change in the form. contains some bugs but hey it works

--- controller.js
+++ controller.js
@@ -1,19 +1,71 @@
-new Vue ({
+new Vue({
 
-    el:'#app',
-    data:{
-        message:'Pricing Menu',
-        mStatus:'',
-        child:'',
-        total:'',
-        roomType:'',
-        counter:0
+    el: '#app',
+    data: {
+        message: 'Pricing Menu',
+        mStatus:
+            {
+                single: 200,
+                married: 500,
+                title: '',
+                price: null
+            },
+        roomType: {
+            bronze: 1000,
+            silver: 2000,
+            gold: 3000,
+            title: '',
+            price: null
+        },
+        alert: {
+            show: false,
+            type: 'info',
+            message: '',
+        },
+
+
+        childValue: 0,
+
+        counter: 0
+
 
     },
 
-    method:{
+    mounted: {},
 
-    }
+    methods: {
+        increment() {
+            this.counter += 20;
+        },
+        decrement() {
+            this.counter -= 20;
+        },
+        mPrice() {
+            if (this.mStatus.title === 'married')
+                return this.mStatus.price = this.mStatus.married;
+
+            if (this.mStatus.title === 'single')
+                return this.mStatus.price = this.mStatus.single;
+
+        },
+
+        rPrice() {
+            if (this.roomType.title === 'gold')
+                return this.roomType.price = this.roomType.gold;
+
+            if (this.roomType.title === 'silver')
+                return this.roomType.price = this.roomType.silver;
+
+            if (this.roomType.title === 'bronze')
+                return this.roomType.price = this.roomType.bronze;
+
+
+        },
+
+
+    },
+
+
 
 })
 
